2232b838b37856831d0faa3b55628b9495f9e53e
Kaique Mendonca <kai_que15@hotmail.com>
2019-10-11 22:00:26 -0300

    Adicionado todos os arquivos do site DIYS

---
+++ projetox/HTML/index.php
@@ -0,0 +1,89 @@
+<html>
+    <head>
+    <meta  charset="UTF-8">
+    <meta id="viewport" name="viewport" content="width=device-width, user-scalable=no">
+    <title>DIYS - NUNCA FOI TÃO FÁCIL SER BONITO</title>
+    <link type="text/css" href="padrao.css" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="normalize.css-master/normalize.css" />
+    
+    <?php
+                if(isset(%_GET['pes']))
+                {
+                    echo "Pesquisa feita!"
+                }
+     ?>
+    </head>
+    <body>
+        <header>
+            <div class="headerin">
+                <div class="logomarca">
+                    <a href="index.html"><img src="diys2.png"  /></a>
+                    
+                </div>
+                
+                <div class="login">
+                    <a href="">Entrar</a>
+                </div>
+            </div>
+        </header>
+        
+        <nav>
+            <div class="es">
+                <img src="instrumentos.png">
+            </div>
+            
+            <div class="dentro">
+                
+
+                <nav class="search">
+                    <h1>Os melhores na palma da sua mão</h1>
+                    <form method="GET"> 
+                        <input type="text" aria-autocomplete="list" placeholder="Insira o seu endereço" spellcheck="false" aria-labelledby="location-enter-address-label" autocomplete="on" autocorrect="off" name="userQuery" required/>
+                        <input class="buttonpes" name="pes" type="button" value="Pesquisar">
+                    </form>
+                </nav>
+             </div>
+            
+            <div class="di">
+                <img src="cadeira.png">
+            </div>
+        </nav>
+        
+        <footer>
+            <div class="footertop">
+                
+                <div  class="footertopleft">
+                
+                    <div  class="logomarca2">
+                        <a href="index.html"><img src="diys2.png"  /></a>
+                    </div >
+                    
+                    <div  class="download">
+                        <a href="https://itunes.apple.com/app/id1058959277?my_keyword=ubereatscom_iOScta_footer&amp;ref_id=754709844.1566922126&amp;sub1=06ff3e37-e8fa-4ffd-8b22-bae7c18189b2"><img src="https://d3i4yxtzktqr9n.cloudfront.net/web-eats-v2/fdfc5304d9819be57b4783c6bef2e118.svg" class="cf"></a>
+                        
+                        <a href="https://play.google.com/store/apps/details?id=com.ubercab.eats&amp;my_keyword=ubereatscom_androidcta_footer&amp;ref_id=754709844.1566922126&amp;sub1=06ff3e37-e8fa-4ffd-8b22-bae7c18189b2"><img src="https://d3i4yxtzktqr9n.cloudfront.net/web-eats-v2/b3b3f379a592f6067b005dade511acc9.png" class="f3"></a>
+                    </div >
+                    
+                </div >
+                
+                <div  class="footertopright">
+                    <a class="about" href="">Sobre a DIYS</a>
+                    <a href="">Leia nosso blog</a>
+                    <a href="">Cadastre-se para fazer entregas</a>
+                    <a href="">Adicione seu espaço de beleza</a>
+                    
+                </div >
+                
+                <div  class="footertopright">
+                    <a class="about" href="">Ajuda</a>
+                    <a href="">Leia as perguntas frequentes</a>
+                    <a href="">Veja todas as cidades</a>
+                    
+                </div >
+                
+            </div >
+            
+            
+        </footer>
+    </body>
+</html>
